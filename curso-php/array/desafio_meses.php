1e9afd4c134396d98443669407202edc0147d11f
vinicius-permor <ma.vinicius.moraes@gmail.com>
2023-10-24 08:50:45 -0300

    deslocamento de array

---
+++ curso-php/array/desafio_meses.php
@@ -0,0 +1,13 @@
+<div class="tiulo">Desafio Meses</div>
+
+<?php
+$meses = array( 1 =>"janeiro", "fevereiro",   // fazendo o deslocamento do primeiro indice os seguintes segue e sequencia numerica 
+ "março", "abriu",
+  "maio", "junho",
+   "julho", "agosto",
+    "setembro", "outubro",
+     "novembro", "dezembro");
+
+print_r($meses);
+echo '<br>' . $meses[5];
+echo '<br>' . $meses[12];
--- curso-php/index.php
+++ curso-php/index.php
@@ -196,6 +196,16 @@
                                 Mapa
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=array&file=desafio_index">
+                                Desafio Index
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=array&file=desafio_meses">
+                                Desafio Meses
+                            </a>
+                        </li>
 
                     </ul>
                 </div>
