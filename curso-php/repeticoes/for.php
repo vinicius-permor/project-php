f9a9804d73d2fbc6050af850dbd1a2a5f1fe7f29
vinicius-permor <ma.vinicius.moraes@gmail.com>
2023-10-25 14:04:45 -0300

    laço For

--- curso-php/repeticoes/for.php
+++ curso-php/repeticoes/for.php
@@ -1,3 +1,44 @@
 <div class="titulo">Laço For</div>
 
 <?php
+
+for ($cont=1; $cont <=5 ; $cont++) { 
+  print "$cont <br>";
+};
+
+echo "<hr>";
+
+for (;$cont <= 10; $cont++) { 
+  print "$cont <br>";
+};
+
+echo "<hr>";
+
+for(; $cont <= 15;) {
+  print "$cont <br>";
+  $cont++;
+};
+
+$array = ['segunda', 'terça', 'quarta', 'quinta', 'sexta'];
+
+echo "<hr>";
+print_r($array);
+echo '<br>';
+
+echo "<hr>";
+for($i = 0; $i < count($array); $i++) {
+  echo "{$array[$i]} <br>";
+};
+
+
+$matrix = [
+  ['a','e','i','o','u'],
+  ['b','c','d']
+];
+
+for($i = 0; $i <  count($matrix); $i++) {
+    for($j = 0; $j < count($matrix[$i]); $j++) {
+      print "{$matrix[$i][$j]} ";
+    }
+    echo "<br>";
+}
