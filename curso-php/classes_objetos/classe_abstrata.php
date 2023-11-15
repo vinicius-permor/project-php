79f33cf68782d08bde86cf5575461f6b34f25d64
vinicius-permor <ma.vinicius.moraes@gmail.com>
2023-11-15 11:41:42 -0300

    interface

--- curso-php/classes_objetos/classe_abstrata.php
+++ curso-php/classes_objetos/classe_abstrata.php
@@ -1,3 +1,44 @@
 <div class="titulo"> Classe Abstrata </div>
 
 <?php
+
+abstract class SuperClasse
+{
+    public abstract function novafuncao();
+    abstract protected function outranova_funcao($parametro);
+}
+
+abstract class ClasseSecundaria extends SuperClasse {
+    public function novafuncao()
+    {
+        echo "usando metodo da função publica nova função<br>";
+    }
+
+    abstract public function funcaoDaClasse_secundaria();
+}
+class Concreta extends ClasseSecundaria {
+    public function novafuncao() {
+        echo "executando o metodo da novafuncao <br>";
+        parent::novafuncao();
+    }
+    protected function outranova_funcao($parametro) {
+        echo "execurtando outranova_funcao na classe concreta, $parametro<br>";
+    }
+    public function funcaoDaClasse_secundaria() {
+        echo "executando metodo da funcao da classe secundaria , na exrtensão com a classe concreta<br>";
+        $this->outranova_funcao('um novo parametro');
+    }
+}
+
+
+$item = new Concreta();
+$item->novafuncao();
+$item->funcaoDaClasse_secundaria();
+
+
+
+
+
+
+
+
